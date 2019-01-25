63d08e5744adf806ea1b5a5b4ca62acfea5ac678
Danis_Alifian <danisalifian@gmail.com>
2019-01-26 00:35:00 +0700

    Auto stash before merge of "master" and "origin/master"

--- resources/views/pages/ticket.blade.php
+++ resources/views/pages/ticket.blade.php
@@ -1,10 +1,20 @@
 @extends('layout.master')
 @section('content')
+<br>
+<div class="container">
+    <h2> Pilih Tempat Duduk</h2><hr>
 
-    <h2> Pilih Tempat Duduk</h2>
+    <div class ="row justify-content-center">
+    
+    <div class ="col-9">
+    
+        <div class="alert alert-primary">
+            <h6 class="text-center">Main Screen</h6>
+        </div><br><br><br>
 
-    <div>
-        <ol class="seats">
+    <div class="col-md-6 offset-md-1"> 
+        <!-- ROW Pertama A -->
+        <ol class="seats" style="width: 0px;">
             <li class="seat"> 
                 <input type="checkbox" id="A1" checked onclick="return false;"/>
                 <label for="A1">A1</label>
@@ -19,14 +29,427 @@
                 <input type="checkbox" id="A3"/>
                 <label for="A3">A3</label>
             </li>
-        </ol>
 
-        <ol class="seats">
             <li class="seat"> 
-                <input type="checkbox" id="B1"/>
-                <label for="B1">B1</label>
+                <input type="checkbox" id="A4"/>
+                <label for="A4">A4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="A5"/>
+                <label for="A5">A5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A6"/>
+                <label for="A6">A6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A7"/>
+                <label for="A7">A7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A8"/>
+                <label for="A8">A8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A9"/>
+                <label for="A9">A9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A10"/>
+                <label for="A10">A10</label>
             </li>
         </ol>
+        <!-- END ROW Pertama A -->
+
+        <!-- ROW Pertama B -->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="B1" checked onclick="return false;"/>
+                <label for="B1">B1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="B2"/>
+                <label for="B2">B2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="B3"/>
+                <label for="B3">B3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="B4"/>
+                <label for="B4">B4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id=B5"/>
+                <label for="B5">B5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="B6"/>
+                <label for="B6">B6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="B7"/>
+                <label for="B7">B7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A8"/>
+                <label for="B8">B8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A9"/>
+                <label for="B9">B9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="A10"/>
+                <label for="B10">B10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama B -->
+
+        <!-- ROW Pertama C-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="C1"/>
+                <label for="C1">C1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C2"/>
+                <label for="C2">C2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C3"/>
+                <label for="C3">C3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C4"/>
+                <label for="C4">C4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="C5"/>
+                <label for="C5">C5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C6"/>
+                <label for="C6">C6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C7"/>
+                <label for="C7">C7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C8"/>
+                <label for="C8">C8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C9"/>
+                <label for="C9">C9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C10"/>
+                <label for="C10">C10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama C -->
+
+        <!-- ROW Pertama D-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="D1"/>
+                <label for="D1">D1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D2"/>
+                <label for="D2">D2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D3"/>
+                <label for="D3">D3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="C4"/>
+                <label for="D4">D4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="D5"/>
+                <label for="D5">D5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D6"/>
+                <label for="D6">D6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D7"/>
+                <label for="D7">D7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D8" checked onclick="return false;"/>
+                <label for="D8">D8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D9"/>
+                <label for="D9">D9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="D10"/>
+                <label for="C10">D10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama D -->
+
+        <!-- ROW Pertama E-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="E1" checked onclick="return false;"/>
+                <label for="E1">E1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E2"/>
+                <label for="E2">E2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E3"/>
+                <label for="E3">E3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E4"/>
+                <label for="E4">E4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="E5"/>
+                <label for="E5">E5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E6"/>
+                <label for="E6">E6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E7" checked onclick="return false;"/>
+                <label for="E7">E7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E8"/>
+                <label for="E8">E8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E9"/>
+                <label for="E9">E9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="E10"/>
+                <label for="E10">E10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama E -->
+
+        <!-- ROW Pertama D-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="F1" checked onclick="return false;"/>
+                <label for="F1">F1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F2"/>
+                <label for="F2">F2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F3"/>
+                <label for="F3">F3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F4"/>
+                <label for="F4">F4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="F5"/>
+                <label for="F5">F5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F6"/>
+                <label for="F6">F6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F7" />
+                <label for="F7">F7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F8"/>
+                <label for="F8">F8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F9"/>
+                <label for="F9">F9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="F10"/>
+                <label for="F10">F10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama F -->
+
+        <!-- ROW Pertama G-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="G1"/>
+                <label for="G1">G1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G2"/>
+                <label for="G2">G2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G3"/>
+                <label for="G3">G3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G4"/>
+                <label for="G4">G4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="G5"/>
+                <label for="G5">G5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G6"/>
+                <label for="G6">G6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G7"/>
+                <label for="G7">G7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G8" checked onclick="return false;"/>
+                <label for="G8">G8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G9"/>
+                <label for="G9">G9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="G10"/>
+                <label for="G10">G10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama G -->
+
+        <!-- ROW Pertama H-->
+        <ol class="seats" style="width: 0px;">
+            <li class="seat"> 
+                <input type="checkbox" id="H1" checked onclick="return false;"/>
+                <label for="H1">E1</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H2"/>
+                <label for="H2">H2</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H3"/>
+                <label for="H3">H3</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H4"/>
+                <label for="H4">H4</label>
+            </li>
+
+            <li class="seat" style="margin-right: 40px"> 
+                <input type="checkbox" id="H5"/>
+                <label for="H5">H5</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H6" checked onclick="return false;"/>
+                <label for="H6">H6</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H7"/>
+                <label for="H7">H7</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H8" checked onclick="return false;"/>
+                <label for="H8">H8</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H9"/>
+                <label for="H9">H9</label>
+            </li>
+
+            <li class="seat"> 
+                <input type="checkbox" id="H10"/>
+                <label for="H10">H10</label>
+            </li>
+        </ol>
+        <!-- END ROW Pertama H -->
+
+    </div>
+    </div>
     </div>
 
+    <br><br>
+
+</div>
 @endsection
