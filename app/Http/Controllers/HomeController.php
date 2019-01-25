3051e69e5cf57c607d904a094dcc11ca05730008
alesna <alesna97@gmail.com>
2019-01-25 16:20:05 +0700

    'set-database'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $data['poster'] = DB::select('select * from users where active = ?', [1])
-        return view('welcome');
+        $data['poster'] = DB::select('select * from tb_movie');
+        
+        return view('welcome', $data);
     }
 }
